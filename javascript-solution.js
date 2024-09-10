487f33f952d21244b530c099ada91e814b1583c1
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-10 18:12:46 +0600

    Calculate money for ZOO

---
+++ javascript-solution.js
@@ -0,0 +1,13 @@
+// Calculate money for ZOO
+/*
+*function calculateMoney(soldTicket) {
+*  if (soldTicket < 0) {
+*    return "Invalid Number";
+*  }
+*  let income = soldTicket * 120 - (500 + 8 * 50);
+*  return income;
+*}
+*
+*console.log(calculateMoney(93));
+ */
+
