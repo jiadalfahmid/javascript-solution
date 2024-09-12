9f2bdbf013c074cc0401bf02452975ed8127a163
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-12 22:10:55 +0600

    Good Name Bad Name finder

--- javascript-solution.js
+++ javascript-solution.js
@@ -1,13 +1,23 @@
 // Calculate money for ZOO
-/*
-*function calculateMoney(soldTicket) {
-*  if (soldTicket < 0) {
-*    return "Invalid Number";
-*  }
-*  let income = soldTicket * 120 - (500 + 8 * 50);
-*  return income;
-*}
-*
-*console.log(calculateMoney(93));
- */
 
+function calculateMoney(soldTicket) {
+  if (soldTicket < 0) {
+    return "Invalid Number";
+  }
+  let income = soldTicket * 120 - (500 + 8 * 50);
+  return income;
+}
+
+// Good Name Bad Name.
+
+function checkName(name) {
+  let goodNameLastL = "ayieouw";
+  for (const letter of goodNameLastL) {
+    if (typeof name === "string" && name.slice(-1) === letter) {
+      return "Good name";
+    }
+  }
+  return "Bad name";
+}
+
+// 
